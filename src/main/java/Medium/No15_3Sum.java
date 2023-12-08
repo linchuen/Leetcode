519d42baa0d7fc11d1e48f0bcd5af70cb201e81d
Aiden <aiden.lin@ambernet.com.tw>
2023-12-08 10:32:34 +0800

    調整

--- src/main/java/Medium/No15_3Sum.java
+++ src/main/java/Medium/No15_3Sum.java
@@ -59,25 +59,23 @@ public class No15_3Sum {
 
         Arrays.sort(nums);
 
-        for(int i=0; i<len; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int left = i+1;
-            int right = len-1;
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum<0) {
+                if (sum < 0) {
                     left++;
-                }
-                else if(sum>0) {
+                } else if (sum > 0) {
                     right--;
-                }
-                else{
+                } else {
                     answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                    while((left < right) && nums[left]==nums[left-1]) left++;
-                    while((left < right) && nums[right]==nums[right+1]) right--;
+                    while ((left < right) && nums[left] == nums[left - 1]) left++;
+                    while ((left < right) && nums[right] == nums[right + 1]) right--;
                 }
             }
         }
